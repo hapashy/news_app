c9d44a7a8cb72881fc12bb5b73a49768fd99182b
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-05-27 14:18:12 +0300

    finsh app

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({super.key, required this.category});
@@ -7,22 +8,35 @@ class CategoryCard extends StatelessWidget {
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16),
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.fill, image: AssetImage(category.image)),
-          borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return CategoryView(
+                category: category.categoryName,
+              );
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(category.image)),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          width: 160,
+          height: 85,
+          child: Center(
+              child: Text(
+            category.categoryName,
+            style: const TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          )),
         ),
-        width: 160,
-        height: 85,
-        child: Center(
-            child: Text(
-          category.categoryName,
-          style: const TextStyle(
-              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        )),
       ),
     );
   }
